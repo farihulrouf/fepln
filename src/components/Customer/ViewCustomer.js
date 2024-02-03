107d3129b0701993dface5b8d4f0fe9de469f93d
Farihul Rouf <farihulrouf@gmail.com>
2024-02-03 22:58:30 +0700

    qrx

--- src/components/Customer/ViewCustomer.js
+++ src/components/Customer/ViewCustomer.js
@@ -85,84 +85,86 @@ const ViewCustomer = (props) => {
   // console.log(transaction);
   //console.log('data di eksekusi', currentPage)
   //console.log(currentCustomer)
- 
+
   return (
     <React.Fragment>
       {edit ? (
         <EditCustomer customer={currentCustomer} editChange={editChange} />
       ) : (
         <div className="bg-white overflow-hidden shadow rounded-lg border">
-          <div className="px-4 py-5 sm:px-6 flex items-center space-x-2 relative">
-            {isLoading ?
-            ( <Spinner />):(
-              <>
-               <Avatar
-              className="rounded-full"
-              name={currentCustomer.name + " " + currentCustomer.name}
-              maxInitials={2}
-              size={50}
-            />
-            <div>
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
-                {currentCustomer.name}
-              </h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                {currentCustomer.no_id}
-              </p>
-            </div>
-            <MdModeEditOutline
-              size={25}
-              className="absolute right-4"
-              onClick={editChange}
-            />
-              </>
-            )}
-           
-          </div>
-          <div className="border-t border-gray-200 px-4 py-2 sm:p-0">
-            <dl className="sm:divide-y sm:divide-gray-200">
-              <div className="py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <div className="flex items-center justify-between">
-                  <div>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              <div className="px-4 py-5 sm:px-6 flex items-center space-x-2 relative">
+                <Avatar
+                  className="rounded-full"
+                  name={currentCustomer.name + " " + currentCustomer.name}
+                  maxInitials={2}
+                  size={50}
+                />
+                <div>
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">
+                    {currentCustomer.name}
+                  </h3>
+                  <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                    {currentCustomer.no_id}
+                  </p>
+                </div>
+                <MdModeEditOutline
+                  size={25}
+                  className="absolute right-4"
+                  onClick={editChange}
+                />
+              </div>
+              <div className="border-t border-gray-200 px-4 py-2 sm:p-0">
+                <dl className="sm:divide-y sm:divide-gray-200">
+                  <div className="py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <dt className="text-sm font-medium text-gray-500">
+                          Gender
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                          {currentCustomer.gender == "L" ? (
+                            <p>Male</p>
+                          ) : (
+                            <>Female</>
+                          )}
+                        </dd>
+                      </div>
+
+                      <div>
+                        <dt className="text-sm font-medium text-gray-500">
+                          Phone number
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex items-center">
+                          <FaWhatsapp size={25} className="text-teal-700" />{" "}
+                          <p>{currentCustomer.no_tel} </p>
+                        </dd>
+                      </div>
+
+                      <QRCode
+                        size={50}
+                        value={String(currentCustomer.no_id)}
+                        viewBox={`0 0 256 256`}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt className="text-sm font-medium text-gray-500">
-                      Gender
+                      Address
                     </dt>
                     <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                      {currentCustomer.gender == "L" ? (
-                        <p>Male</p>
-                      ) : (
-                        <>Female</>
-                      )}
-                    </dd>
-                    
-                  </div>
-
-                  <div>
-                    <dt className="text-sm font-medium text-gray-500">
-                      Phone number
-                    </dt>
-                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex items-center">
-                      <FaWhatsapp size={25} className="text-teal-700" />{" "}
-                      <p>{currentCustomer.no_tel} </p>
+                      {currentCustomer.addres}
                     </dd>
                   </div>
-
-                  <QRCode
-                      size={50}
-                      value={String(currentCustomer.no_id)}
-                      viewBox={`0 0 256 256`}
-                    />
-                </div>
+                </dl>
               </div>
+            </>
+          )}
 
-              <div className="py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Address</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {currentCustomer.addres}
-                </dd>
-              </div>
-            </dl>
-          </div>
           <div className="flex flex-col">
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
