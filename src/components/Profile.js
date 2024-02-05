4540528b05a6a41b693c83a4e7dca78ceb626254
Farihul Rouf <farihulrouf@gmail.com>
2024-02-05 17:11:22 +0700

    qr

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,31 +7,31 @@ import ServiceApi from "../services/ServiceApi";
 const Profile = () => {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-  const [err, setErr] = useState('');
+  const [err, setErr] = useState("");
   const [decodedValue, setDecodedValue] = useState(39843024);
   const [scannerType, setScannerType] = useState("QR");
 
-  const [idvalue, setIdvalue] = useState("")
-  const [customer, setCustomer] = useState(null)
+  const [idvalue, setIdvalue] = useState("");
+  const [customer, setCustomer] = useState(null);
   const handleClick = async () => {
     const params = {
-      id: decodedValue
+      id: decodedValue,
       //parseInt(nomer, 10),
     };
-    setIsLoading(true)
+    setIsLoading(true);
     ServiceApi.getNoCustomer(params)
-    .then((response) => {
-     // console.log(response)
-      setCustomer(response.data)
-      setIsLoading(false)
-     // setCurrentCustomer(response.data);
-     // setLoad(false);
+      .then((response) => {
+        // console.log(response)
+        setCustomer(response.data);
+        setIsLoading(false);
+        // setCurrentCustomer(response.data);
+        // setLoad(false);
 
-      //console.log(response.data);
-    })
-    .catch((e) => {
-      setIsLoading(false)
-    });
+        //console.log(response.data);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+      });
   };
 
   console.log(customer);
@@ -40,20 +40,13 @@ const Profile = () => {
     <div className="px-6 py-2">
       {err && <h2>{err}</h2>}
       <div className="py-4">
-      {customer? (
-       <>
-         {customer.user.name}
-       </>
-      ):(
-       <>
-         Ok
-       </>
-      )}
+        {customer ? <> Data terbaca </> : <>Ok</>}
       </div>
-      <button className="px-2 py-2 p-2 bg-pink-500" onClick={handleClick}>Fetch data</button>
+      <button className="px-2 py-2 p-2 bg-pink-500" onClick={handleClick}>
+        Fetch data
+      </button>
 
       {isLoading && <h2>Loading...</h2>}
-     
     </div>
   );
 };
