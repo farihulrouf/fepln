2ce1eaaa28cb95549f64a3059b883e67dd348382
Farihul Rouf <farihulrouf@gmail.com>
2024-02-05 09:50:37 +0700

    qr

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,50 +6,38 @@ import axios from "axios";
 import ServiceApi from "../services/ServiceApi";
 import { Html5QrcodeScanner } from "html5-qrcode";
 const Profile = () => {
-  const [scannedCodes, setScannedCodes] = useState([]);
-
-  function activateLasers() {
-    var decodedText = "asdf";
-    var decodedResult = "asdfasdfasdf";
-    console.log(scannedCodes);
-
-    setScannedCodes(scannedCodes.concat([{ decodedText, decodedResult }]));
-  }
+  const [scanResult, setScanResult] = useState(null);
 
   useEffect(() => {
-    function onScanSuccess(decodedText, decodedResult) {
-      // handle the scanned code as you like, for example:
-      console.log(`Code matched = ${decodedText}`, decodedResult);
-      setScannedCodes(scannedCodes.concat([{ decodedText, decodedResult }]));
-    }
+    const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
 
-    function onScanFailure(error) {
-      // handle scan failure, usually better to ignore and keep scanning.
-      // for example:
-      console.warn(`Code scan error = ${error}`);
-    }
+    scanner.render(success, error);
 
-    let html5QrcodeScanner = new Html5QrcodeScanner(
-      "reader",
-      { fps: 10, qrbox: { width: 250, height: 250 } },
-      /* verbose= */ false
-    );
-    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    function success (result) {
+      scanner.clear();
+      setScanResult(result);
+    };
+
+    function error (err) {
+      console.warn(err);
+    };
   });
 
   return (
     <div>
-      <h1>Where is Starman</h1>
-      <p>description</p>
-      <div id="reader" width="600px"></div>
-      <ol>
-        {scannedCodes.map((scannedCode, index) => (
-          <li key={index}>{scannedCode.decodedText}</li>
-        ))}
-      </ol>
-      <button onClick={activateLasers}>Activate Lasers</button>
+      { scanResult 
+      ? <div> success: {scanResult} </div>
+      : <div id="reader"></div>
+      }
     </div>
   );
+
   {
     /*
   const currentUser = AuthService.getCurrentUser();
