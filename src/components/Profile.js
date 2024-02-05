c3900b096472e5f012ea3f13e3afd8c4d51d623d
Farihul Rouf <farihulrouf@gmail.com>
2024-02-05 17:05:33 +0700

    qr

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -39,10 +39,6 @@ const Profile = () => {
   return (
     <div className="px-6 py-2">
       {err && <h2>{err}</h2>}
-
-      <button className="px-2 py-2 p-2 bg-pink-500" onClick={handleClick}>Fetch data</button>
-
-      {isLoading && <h2>Loading...</h2>}
       <div className="py-4">
       {customer? (
        <>
@@ -54,6 +50,10 @@ const Profile = () => {
        </>
       )}
       </div>
+      <button className="px-2 py-2 p-2 bg-pink-500" onClick={handleClick}>Fetch data</button>
+
+      {isLoading && <h2>Loading...</h2>}
+     
     </div>
   );
 };
