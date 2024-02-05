2051932d59787edc34f63c8bd335a912444c4fff
Farihul Rouf <farihulrouf@gmail.com>
2024-02-05 20:50:34 +0700

    tx

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,6 +6,7 @@ import CardProfile from "./CardProfile";
 import Spinner from "./Spinner";
 import ServiceApi from "../services/ServiceApi";
 import Scanner from "./Scanner";
+import Transaction from "./Transaction";
 const Profile = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState("");
@@ -86,7 +87,16 @@ const Profile = () => {
         {isLoading ? (
           <Spinner />
         ) : (
-          <>{customer ? <CardProfile customer={customer} /> : <></>}</>
+          <>
+            {customer ? (
+              <React.Fragment>
+                <CardProfile customer={customer} />
+                <Transaction customer={customer} />
+              </React.Fragment>
+            ) : (
+              <></>
+            )}
+          </>
         )}
       </div>
       {/*
@@ -95,81 +105,6 @@ const Profile = () => {
       </button>
       {isLoading && <h2>Loading...</h2>}
         */}
-      <div className="py-2">
-        <label
-          for="quantity-input"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Meteran:
-        </label>
-        <div className="relative flex items-center w-36">
-          <button
-            type="button"
-            id="decrement-button"
-            data-input-counter-decrement="quantity-input"
-            className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
-          >
-            <svg
-              className="w-3 h-3 text-gray-900 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 2"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h16"
-              />
-            </svg>
-          </button>
-          <input
-            type="number"
-            id="quantity-input"
-            data-input-counter
-            aria-describedby="helper-text-explanation"
-            className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="999"
-            required
-          />
-          <button
-            type="button"
-            id="increment-button"
-            data-input-counter-increment="quantity-input"
-            className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
-          >
-            <svg
-              className="w-3 h-3 text-gray-900 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
-          </button>
-        </div>
-        <div class="flex flex-row items-center justify-between py-4">
-          <div class="flex flex-col items-start">
-            <span class="text-sm font-medium text-gray-600">Credit Limit</span>
-            <span class="text-lg font-medium text-gray-800">$10,000</span>
-          </div>
-          <div class="flex flex-col items-start">
-            <span class="text-sm font-medium text-gray-600">
-              Available Balance
-            </span>
-            <span class="text-lg font-medium text-gray-800">$7,500</span>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
