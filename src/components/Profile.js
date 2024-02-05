53d78b4cd57c85ea169d85cdadb637515923a4aa
Farihul Rouf <farihulrouf@gmail.com>
2024-02-05 17:25:13 +0700

    qr

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -22,7 +22,7 @@ const Profile = () => {
     ServiceApi.getNoCustomer(params)
       .then((response) => {
         // console.log(response)
-        setCustomer(response.data);
+        setCustomer(response.data.user);
         setIsLoading(false);
         // setCurrentCustomer(response.data);
         // setLoad(false);
@@ -47,8 +47,9 @@ const Profile = () => {
       {err && <h2>{err}</h2>}
       <div className="py-4">
         {customer ? 
-        <> { customer.user.name } </> : 
-        <>Ok</>}
+        <> { customer.name } </> : 
+        <>Ok</>
+        }
       </div>
       <button className="px-2 py-2 p-2 bg-pink-500" onClick={handleClick}>
         Fetch data
