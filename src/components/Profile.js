199be4b00f6c25b19ae68992f4d9f545e3e7c263
Farihul Rouf <farihulrouf@gmail.com>
2024-02-05 20:40:36 +0700

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
-import Avatar from "react-avatar";
-import { MdModeEditOutline } from "react-icons/md";
-import { FaWhatsapp } from "react-icons/fa";
-import QRCode from "react-qr-code";
+import { FaTachometerAlt } from "react-icons/fa";
+
 import CardProfile from "./CardProfile";
 // import { getCameraList } from "./Utils";
 import Spinner from "./Spinner";
@@ -85,19 +83,11 @@ const Profile = () => {
       {/*<input type="number" onChange={onChangeData} /> */}
       {err && <h2>{err}</h2>}
       <div className="py-4">
-        <>
-          {isLoading ? (
-            <Spinner />
-          ) : (
-            <>
-              {customer ? (
-                <CardProfile customer={customer} />
-              ) : (
-                <></>
-              )}
-            </>
-          )}
-        </>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>{customer ? <CardProfile customer={customer} /> : <></>}</>
+        )}
       </div>
       {/*
       <button className="px-2 py-2 p-2 bg-pink-500" onClick={handleClick}>
@@ -105,6 +95,81 @@ const Profile = () => {
       </button>
       {isLoading && <h2>Loading...</h2>}
         */}
+      <div className="py-2">
+        <label
+          for="quantity-input"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Meteran:
+        </label>
+        <div className="relative flex items-center w-36">
+          <button
+            type="button"
+            id="decrement-button"
+            data-input-counter-decrement="quantity-input"
+            className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+          >
+            <svg
+              className="w-3 h-3 text-gray-900 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 2"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h16"
+              />
+            </svg>
+          </button>
+          <input
+            type="number"
+            id="quantity-input"
+            data-input-counter
+            aria-describedby="helper-text-explanation"
+            className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="999"
+            required
+          />
+          <button
+            type="button"
+            id="increment-button"
+            data-input-counter-increment="quantity-input"
+            className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+          >
+            <svg
+              className="w-3 h-3 text-gray-900 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 18"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 1v16M1 9h16"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="flex flex-row items-center justify-between py-4">
+          <div class="flex flex-col items-start">
+            <span class="text-sm font-medium text-gray-600">Credit Limit</span>
+            <span class="text-lg font-medium text-gray-800">$10,000</span>
+          </div>
+          <div class="flex flex-col items-start">
+            <span class="text-sm font-medium text-gray-600">
+              Available Balance
+            </span>
+            <span class="text-lg font-medium text-gray-800">$7,500</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
