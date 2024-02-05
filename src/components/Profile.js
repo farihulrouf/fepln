6587e8e17e00a5dab224e002c08f96da78cff452
Farihul Rouf <farihulrouf@gmail.com>
2024-02-05 09:21:26 +0700

    qr

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,19 +4,81 @@ import Spinner from "./Spinner";
 import Cardsearch from "./Cardsearch";
 import axios from "axios";
 import ServiceApi from "../services/ServiceApi";
-import {QrScanner} from '@yudiel/react-qr-scanner';
+import { Html5QrcodeScanner } from "html5-qrcode";
 const Profile = () => {
+  const [scannedCodes, setScannedCodes] = useState([]);
+
+  function activateLasers() {
+    var decodedText = "asdf";
+    var decodedResult = "asdfasdfasdf";
+    console.log(scannedCodes);
+
+    setScannedCodes(scannedCodes.concat([{ decodedText, decodedResult }]));
+  }
+
+  useEffect(() => {
+    function onScanSuccess(decodedText, decodedResult) {
+      // handle the scanned code as you like, for example:
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+      setScannedCodes(scannedCodes.concat([{ decodedText, decodedResult }]));
+    }
+
+    function onScanFailure(error) {
+      // handle scan failure, usually better to ignore and keep scanning.
+      // for example:
+      console.warn(`Code scan error = ${error}`);
+    }
+
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+      "reader",
+      { fps: 10, qrbox: { width: 250, height: 250 } },
+      /* verbose= */ false
+    );
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+  });
+
+  return (
+    <div>
+      <h1>Where is Starman</h1>
+      <p>description</p>
+      <div id="reader" width="600px"></div>
+      <ol>
+        {scannedCodes.map((scannedCode, index) => (
+          <li key={index}>{scannedCode.decodedText}</li>
+        ))}
+      </ol>
+      <button onClick={activateLasers}>Activate Lasers</button>
+    </div>
+  );
+  {
+    /*
   const currentUser = AuthService.getCurrentUser();
   const [nomer, setNomer] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [searchShow, setSearchShow] = useState(false);
   const [resultdata, setResultdata] = useState();
   const [price, setPrice] = useState([]);
+  
+  const [scanResult, setScanResult] = useState(null)
 
-  //const [showinvoice, setShowinvoice] = useState (false)
 
   useEffect(() => {
-    getPrice();
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250
+      },
+      fps: 5
+    })
+    const success = (result) => {
+      scanner.clear()
+      setScanResult(result)
+    }
+  
+    const error = (err) => {
+      console.warn(err)
+    }
+    //getPrice();
   }, []);
 
   const getPrice = () => {
@@ -88,6 +150,8 @@ const Profile = () => {
 
   const transAction = async () => {};
 
+
+
   const resultSearch = () => {
     if (searchShow) {
       return (
@@ -118,23 +182,15 @@ const Profile = () => {
           </button>
         </div>
       </form>
-      <QrScanner
-          onDecode={(result) => console.log(result)}
-          onError={(error) => console.log(error?.message)}
-      />
-      {/*
-      <div className="relative">
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <React.Fragment>
-            <React.Fragment> {resultSearch()}</React.Fragment>{" "}
-          </React.Fragment>
-        )}
-      </div>
-      */}
+      <h1>Qr Code Scaning in React</h1>
+      { scanResult 
+      ? <div>Success: {scanResult} </div>
+      : <div>ok</div>}
+      
     </div>
   );
+      */
+  }
 };
 
 export default Profile;
