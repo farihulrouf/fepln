5a259177393a411e3dbaaafae7a1d7a0f29174c3
Farihul Rouf <farihulrouf@gmail.com>
2024-02-06 09:35:34 +0700

    dx

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,14 +16,14 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="max-w-sm mx-auto bg-teal-500 flex justify-between items-center p-4 shadow-sm">
+    <div className="max-w-sm mx-auto flex justify-between items-center p-4 shadow-sm">
       {/* Left side */}
       <div className="flex items-center">
         <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-          Best <span className="font-bold">Eats</span>
+          Bum <span className="font-bold">Des</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-black text-white rounded-full p-2">Delivery</p>
@@ -63,7 +63,7 @@ const Navbar = () => {
           className="absolute right-4 top-4 cursor-pointer"
         />
         <h2 className="text-2xl p-4">
-          Best <span className="font-bold">Eats</span>
+          Bum <span className="font-bold">Des</span>
         </h2>
         <nav>
           <ul className="flex flex-col p-4 text-gray-800">
