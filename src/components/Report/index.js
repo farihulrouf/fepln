aab542ecf4b48e32bcef4b07da1db5ccb42fa3b3
Farihul Rouf <farihulrouf@gmail.com>
2024-02-09 17:58:13 +0700

    css-date

--- src/components/Report/index.js
+++ src/components/Report/index.js
@@ -8,45 +8,55 @@ const Report = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="">
-      <div className="flex justify-end">
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => {
-              setMonthPicker(false);
-              setStartDate(date);
-            }}
-            dateFormat="MM/yyyy"
-            showMonthYearPicker
-            showFullMonthYearPicker
-          />
+      <DatePicker
+        className="px-2 py-2"
+        selected={startDate}
+        onChange={(date) => {
+          setMonthPicker(false);
+          setStartDate(date);
+        }}
+        dateFormat="MM/yyyy"
+        showMonthYearPicker
+        showFullMonthYearPicker
+      />
+
+      <DatePicker
+        className="px-2 py-2"
+        selected={startDate}
+        onChange={(date) => {
+          setMonthPicker(false);
+          setStartDate(date);
+        }}
+        dateFormat="MM/yyyy"
+        showMonthYearPicker
+        showFullMonthYearPicker
+      />
+
+      <div className="flex items-center bg-white rounded-sm overflow-hidden">
+        <div className="p-4">
+          <IoCashSharp size={45} />
+        </div>
+        <div className="px-4 text-gray-700">
+          <h3 className="text-sm tracking-wider">Cash Balance</h3>
+          <p className="text-3xl">12,768</p>
+        </div>
       </div>
-      <div className="flex flex-col gap-4 mt-4">
-        <div className="flex items-center bg-white rounded-sm overflow-hidden">
-          <div className="p-4">
-            <IoCashSharp size={45} />
-          </div>
-          <div className="px-4 text-gray-700">
-            <h3 className="text-sm tracking-wider">Cash Balance</h3>
-            <p className="text-3xl">12,768</p>
-          </div>
+      <div className="flex items-center bg-white rounded-sm overflow-hidden">
+        <div className="p-4">
+          <FaWater size={45} />
         </div>
-        <div className="flex items-center bg-white rounded-sm overflow-hidden">
-          <div className="p-4">
-            <FaWater size={45} />
-          </div>
-          <div className="px-4 text-gray-700">
-            <h3 className="text-sm tracking-wider">Total Usage</h3>
-            <p className="text-3xl">39,265</p>
-          </div>
+        <div className="px-4 text-gray-700">
+          <h3 className="text-sm tracking-wider">Total Usage</h3>
+          <p className="text-3xl">39,265</p>
         </div>
-        <div className="flex items-center bg-white rounded-sm overflow-hidden">
-          <div className="p-4">
-            <IoCashSharp size={45} />
-          </div>
-          <div className="px-4 text-gray-700">
-            <h3 className="text-sm tracking-wider">Cash Balance</h3>
-            <p className="text-3xl">12,768</p>
-          </div>
+      </div>
+      <div className="flex items-center bg-white rounded-sm overflow-hidden">
+        <div className="p-4">
+          <IoCashSharp size={45} />
+        </div>
+        <div className="px-4 text-gray-700">
+          <h3 className="text-sm tracking-wider">Cash Balance</h3>
+          <p className="text-3xl">12,768</p>
         </div>
       </div>
     </div>
