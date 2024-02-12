902335b7a475a973e7f8fdb7877f8b0139970160
Farihul Rouf <farihulrouf@gmail.com>
2024-02-12 09:28:10 +0700

    name

--- src/components/ListTransaction/index.js
+++ src/components/ListTransaction/index.js
@@ -44,93 +44,107 @@ const ListTransaction = () => {
           {loading ? (
             <Spinner />
           ) : (
-            <table className="min-w-full divide-y divide-gray-200 overflow-x-auto">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Pemakaian
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Status
-                  </th>
-                  {/*
-            <th
-              scope="col"
-              className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Actions
-            </th>
-            */}
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {transaction?.map((trans, index) => {
-                  return (
-                    <tr>
-                      <td className="py-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          {/*
-                 <div className="flex-shrink-0 h-10 w-10">
-                   <img
-                     className="h-10 w-10 rounded-full"
-                     src="https://i.pravatar.cc/150?img=1"
-                     alt=""
-                   />
-                 </div>
-                  */}
-                          <div className="">
-                            <div className="text-sm font-medium text-gray-900">
-                              {trans.customers[0].name}
-                            </div>
-                            <div className="text-sm text-gray-500">
-                              INV: {trans.noinv}
+            <div>
+              <input
+                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 mt-4 mb-4 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder="Nama Customer"
+                aria-label="Meteran"
+              />
+              <table className="min-w-full divide-y divide-gray-200 overflow-x-auto">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Pemakaian
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Status
+                    </th>
+                    {/*
+              <th
+                scope="col"
+                className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Actions
+              </th>
+              */}
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {transaction?.map((trans, index) => {
+                    return (
+                      <tr>
+                        <td className="py-2 whitespace-nowrap">
+                          <div className="flex items-center">
+                            {/*
+                  <div className="flex-shrink-0 h-10 w-10">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src="https://i.pravatar.cc/150?img=1"
+                      alt=""
+                    />
+                  </div>
+                    */}
+                            <div className="">
+                              <div className="text-sm font-medium text-gray-900">
+                                {trans.customers[0].name}
+                              </div>
+                              <div className="text-sm text-gray-500">
+                                INV: {trans.noinv}
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </td>
-                      <td className="py-2 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          Rp {trans.amount}
-                        </div>
-                        <div className="text-sm text-gray-500">
-                          {trans.meteran} Kubik
-                        </div>
-                      </td>
-                      <td className="py-2 whitespace-nowrap">
-                        <span
-                          onClick={onChange}
-                          className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
-                        >
-                          Success
-                        </span>
-                      </td>
-                      {/*
-             <td className="px-6 py-2 whitespace-nowrap  text-sm font-medium">
-               <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                 Edit
-               </a>
-               <a href="#" class="ml-2 text-red-600 hover:text-red-900">
-                 Delete
-               </a>
-             </td>
-             */}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                        </td>
+                        <td className="py-2 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            Rp {trans.amount}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {trans.meteran} Kubik
+                          </div>
+                        </td>
+                        <td className="py-2 whitespace-nowrap">
+                          <span
+                            onClick={onChange}
+                            className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full text-green-800 ${
+                              trans.status ? "bg-green-200" : "bg-red-400"
+                            }`}
+                          >
+                            {trans.status ? (
+                              <span>Success</span>
+                            ) : (
+                              <span>Waiting</span>
+                            )}
+                          </span>
+                        </td>
+                        {/*
+              <td className="px-6 py-2 whitespace-nowrap  text-sm font-medium">
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                  Edit
+                </a>
+                <a href="#" class="ml-2 text-red-600 hover:text-red-900">
+                  Delete
+                </a>
+              </td>
+              */}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           )}
         </>
       )}
