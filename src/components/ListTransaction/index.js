b1a488e195e1f5634b3a7058af284cec6f980d9c
Farihul Rouf <farihulrouf@gmail.com>
2024-02-12 08:15:10 +0700

    spinner

--- src/components/ListTransaction/index.js
+++ src/components/ListTransaction/index.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import CardProfileTest from "../CardProfileTest";
 import TransactionTest from "../TransactionTest";
 import ServiceApi from "../../services/ServiceApi";
+import Spinner from "../Spinner";
 const ListTransaction = () => {
   const [isUpdate, setIsupdate] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -14,8 +15,8 @@ const ListTransaction = () => {
     getData();
   }, []);
 
-  const getData = async() => {
-    setLoading(true)
+  const getData = async () => {
+    setLoading(true);
     ServiceApi.getTransactionsAll(1, 5)
       .then((response) => {
         setTransaction(response.data.transaction[0].data);
@@ -32,7 +33,7 @@ const ListTransaction = () => {
   };
   console.log(transaction);
   return (
-    <>
+    <div className="realtive">
       {isUpdate ? (
         <React.Fragment>
           <CardProfileTest />
@@ -40,28 +41,31 @@ const ListTransaction = () => {
         </React.Fragment>
       ) : (
         <>
-          <table className="min-w-full divide-y divide-gray-200 overflow-x-auto">
-            <thead className="bg-gray-50">
-              <tr>
-                <th
-                  scope="col"
-                  className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Name
-                </th>
-                <th
-                  scope="col"
-                  className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Pemakaian
-                </th>
-                <th
-                  scope="col"
-                  className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Status
-                </th>
-                {/*
+          {loading ? (
+            <Spinner />
+          ) : (
+            <table className="min-w-full divide-y divide-gray-200 overflow-x-auto">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Pemakaian
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  {/*
             <th
               scope="col"
               className="py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
@@ -69,15 +73,15 @@ const ListTransaction = () => {
               Actions
             </th>
             */}
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {transaction?.map((trans, index) => {
-                return (
-                  <tr>
-                    <td className="py-2 whitespace-nowrap">
-                      <div className="flex items-center">
-                        {/*
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {transaction?.map((trans, index) => {
+                  return (
+                    <tr>
+                      <td className="py-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          {/*
                  <div className="flex-shrink-0 h-10 w-10">
                    <img
                      className="h-10 w-10 rounded-full"
@@ -86,29 +90,33 @@ const ListTransaction = () => {
                    />
                  </div>
                   */}
-                        <div className="">
-                          <div className="text-sm font-medium text-gray-900">
-                            Jane Cooper
-                          </div>
-                          <div className="text-sm text-gray-500">
-                            INV: {trans.noinv}
+                          <div className="">
+                            <div className="text-sm font-medium text-gray-900">
+                              Jane Cooper
+                            </div>
+                            <div className="text-sm text-gray-500">
+                              INV: {trans.noinv}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </td>
-                    <td className="py-2 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">Rp {trans.amount}</div>
-                      <div className="text-sm text-gray-500">{trans.meteran} Kubik</div>
-                    </td>
-                    <td className="py-2 whitespace-nowrap">
-                      <span
-                        onClick={onChange}
-                        className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
-                      >
-                        Success
-                      </span>
-                    </td>
-                    {/*
+                      </td>
+                      <td className="py-2 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          Rp {trans.amount}
+                        </div>
+                        <div className="text-sm text-gray-500">
+                          {trans.meteran} Kubik
+                        </div>
+                      </td>
+                      <td className="py-2 whitespace-nowrap">
+                        <span
+                          onClick={onChange}
+                          className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                        >
+                          Success
+                        </span>
+                      </td>
+                      {/*
              <td className="px-6 py-2 whitespace-nowrap  text-sm font-medium">
                <a href="#" class="text-indigo-600 hover:text-indigo-900">
                  Edit
@@ -118,14 +126,15 @@ const ListTransaction = () => {
                </a>
              </td>
              */}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
         </>
       )}
-    </>
+    </div>
   );
 };
 export default ListTransaction;
