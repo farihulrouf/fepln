9ab5166f9de4642970cfb0cd394136cd4a3bc9d0
Farihul Rouf <farihulrouf@gmail.com>
2024-02-19 03:46:37 +0700

    rx

--- src/components/ListTransaction/NewTrans.js
+++ src/components/ListTransaction/NewTrans.js
@@ -10,8 +10,13 @@ import QRCode from "react-qr-code";
 const NewTrans = (nomer) => {
   const [customerData, setcustomerData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [bayar, setBayar] = useState(0);
+  const [kubik, setKubik] = useState(1);
+  const [price, setPrice] = useState(null);
+
   useEffect(() => {
     getTran();
+    getPrice();
   }, []);
 
   const getTran = () => {
@@ -28,7 +33,18 @@ const NewTrans = (nomer) => {
         setLoading(false);
       });
   };
-  console.log("data", customerData.customers[0].name);
+
+  const getPrice = () => {
+    ServiceApi.getallPrice().then((response) => {
+      setPrice(response.data);
+    });
+  };
+
+  const onChanData = (e) => {
+    setKubik(e.target.value);
+    setBayar(((e.target.value) - customerData.meteran )* price[0].harga);
+  };
+  // console.log("data", customerData.customers[0].name);
   return (
     <React.Fragment>
       {customerData === null ? (
@@ -85,9 +101,86 @@ const NewTrans = (nomer) => {
                   />
                 </div>
               </div>
-
-            
             </dl>
+          </div>
+          <div className="py-2 flex justify-between items-center px-4">
+                <h2 className="text-xl">Meteran</h2>
+                <h2 className="text-3xl">{customerData.meteran}</h2>
+          </div>
+          <div className="py-2 flex relative">
+            <div>
+              <label
+                htmlFor="quantity-input"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Meteran:
+              </label>
+              <div className="relative flex items-center w-36">
+                <button
+                  type="button"
+                  id="decrement-button"
+                  data-input-counter-decrement="quantity-input"
+                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                >
+                  <svg
+                    className="w-3 h-3 text-gray-900 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 2"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeWidth="2"
+                      d="M1 1h16"
+                    />
+                  </svg>
+                </button>
+                <input
+                  type="number"
+                  id="quantity-input"
+                  onChange={onChanData}
+                  defaultValue={kubik}
+                  data-input-counter
+                  aria-describedby="helper-text-explanation"
+                  className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="999"
+                  required
+                />
+                <button
+                  type="button"
+                  id="increment-button"
+                  data-input-counter-increment="quantity-input"
+                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                >
+                  <svg
+                    className="w-3 h-3 text-gray-900 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 18"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 1v16M1 9h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="mt-5 absolute right-4">
+              <div className="flex flex-row items-center justify-between py-4">
+                <div className="flex flex-col items-start">
+                  <span className="text-lg font-medium text-gray-800">
+                    Rp {bayar}
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </>
       )}
