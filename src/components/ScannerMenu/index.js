0355f39ffcd62ee23247df55e3b9de30cc8e2057
Farihul Rouf <farihulrouf@gmail.com>
2024-02-19 05:13:21 +0700

    tx

--- src/components/ScannerMenu/index.js
+++ src/components/ScannerMenu/index.js
@@ -105,7 +105,7 @@ const ScannerMenu = ({ user }) => {
           </>
         ) : (
           <>
-            <NewTrans customerData={customer} />
+            
             <LoadingQr />
           </>
         )}
