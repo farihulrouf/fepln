f40aeb2f633cf0da2afbf296eb3b29053cfd4f34
Farihul Rouf <farihulrouf@gmail.com>
2024-02-23 15:55:27 +0700

    rx

--- src/components/LoginComp.js
+++ src/components/LoginComp.js
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
+//  const history = useHistory();
+
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
@@ -52,9 +54,14 @@ const LoginComp = () => {
     form.current.validateAll();
 
     if (checkBtn.current.context._errors.length === 0) {
+      setLoading(true);
       AuthService.login(username, password).then(
         () => {
-          navigate("/dashboard");
+          setLoading(false)
+          window.location.reload('/dashboard');
+          
+          //navigate("/dashboard");
+         /// window.location.reload();
           //window.location.reload();
         },
         (error) => {
@@ -77,97 +84,104 @@ const LoginComp = () => {
   return (
     <React.Fragment>
       {auth ? (
+        //window.location.reload('/dashboard')
         navigate("/dashboard")
       ) : (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <Form className="space-y-6" onSubmit={handleLogin} ref={form}>
-              <div>
-                <label className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    value={username}
-                    onChange={onChangeUsername}
-                    validations={[required]}
-                  />
-                </div>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <>
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                  className="mx-auto h-10 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt="Your Company"
+                />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                  Sign in to your account
+                </h2>
               </div>
+              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <Form className="space-y-6" onSubmit={handleLogin} ref={form}>
+                  <div>
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      Email address
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        value={username}
+                        onChange={onChangeUsername}
+                        validations={[required]}
+                      />
+                    </div>
+                  </div>
 
-              <div>
-                <div className="flex items-center justify-between">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Password
+                      </label>
+                      <div className="text-sm">
+                        <a
+                          href="#"
+                          className="font-semibold text-indigo-600 hover:text-indigo-500"
+                        >
+                          Forgot password?
+                        </a>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={password}
+                        onChange={onChangePassword}
+                        validations={[required]}
+                        autoComplete="current-password"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                      Forgot password?
-                    </a>
+                      Sign in
+                    </button>
                   </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    value={password}
-                    onChange={onChangePassword}
-                    validations={[required]}
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
+                  {message && (
+                    <div className="form-group">
+                      <div className="alert alert-danger" role="alert">
+                        {message}
+                      </div>
+                    </div>
+                  )}
+                  <CheckButton style={{ display: "none" }} ref={checkBtn} />
+                </Form>
 
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
+                <p className="mt-10 text-center text-sm text-gray-500">
+                  Not a member?
+                  <a
+                    href="#"
+                    className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                  >
+                    Create Account
+                  </a>
+                </p>
               </div>
-              {message && (
-                <div className="form-group">
-                  <div className="alert alert-danger" role="alert">
-                    {message}
-                  </div>
-                </div>
-              )}
-              <CheckButton style={{ display: "none" }} ref={checkBtn} />
-            </Form>
-
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?
-              <a
-                href="#"
-                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              >
-                Create Account
-              </a>
-            </p>
-          </div>
+            </>
+          )}
         </div>
       )}
     </React.Fragment>
