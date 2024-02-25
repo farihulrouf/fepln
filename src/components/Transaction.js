9201ee72db2a1937e5c0269a5bab239bb5a0e756
Farihul Rouf <farihulrouf@gmail.com>
2024-02-26 00:36:09 +0700

    prin

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -132,13 +132,15 @@ const Transaction = ({ idtrans, user, setIsupdate }) => {
       });
   };
 
+  const MAX_DATA_SIZE = 125;
+
   const InvoiceColumn = [
-    { width: 10, marginRight: 2, align: "left" },
-    { width: 5, marginRight: 2, align: "center" },
+    { width: 5, marginRight: 2, align: "left" },
+    { width: 10, marginRight: 2, align: "center" },
     { width: 10, align: "right" },
   ];
 
-  const InvoiceColumnHeader = ["date", "Cubic", "Total"];
+  const InvoiceColumnHeader = ["QTY", "Item", "Total"];
 
   const getPrintDeviceList = async () => {
     const nvg = navigator;
@@ -155,28 +157,52 @@ const Transaction = ({ idtrans, user, setIsupdate }) => {
     }
   };
 
-  const sendPrintData = async (characteristic) => {
-    if (characteristic) {
-      console.log("Cache the characteristic", characteristic);
-      const data = [
+  const getData = () => {
+    let invoiceEncoder = new EscPosEncoder();
+    let basePrint = invoiceEncoder
+      .align("center")
+      .line("CODE STORE")
+      .align("left")
+      .line("https://codesandbox.io/")
+      .line(`No Trx: 1234111`)
+      .line(`Kasir   : Zeta`)
+      .line(`Customer: Guest`)
+      .line(`Tanggal   : ${new Date().toTimeString()}`)
+      .line(`Jam       : ${new Date().getHours()}`)
+      .line(`Sub total : ${Number(111111)}`)
+      .line(`Service   : ${Number(1)}`)
+      .line(`Tax       : ${Number(12)}`)
+      .line(`TOTAL     : ${Number(1111111)}`)
+      .newline();
+
+    basePrint
+      .table(InvoiceColumn, [
         InvoiceColumnHeader,
-        ["", "Hippam Wotan", ""],
-        [currentTrans.noinv.toString(), "", currentTrans.no_id.toString()],
-        [
-          moment(new Date()).format("MM/DD/YYYY HH:mm:ss"),
-          kubik.toString(),
-          bayar.toString(),
-        ],
-        ["****", "****", "****"],
-      ];
-      let encoder = new EscPosEncoder();
-      let result = encoder.table(InvoiceColumn, data).encode();
-      // Print text
-      return await characteristic.writeValue(result);
-    } else {
-      throw new Error("characteristic not found!");
-    }
+        ["1", "Green Tea", "11.000"],
+        ["1", "White Tea", "13.000"],
+        ["1", "Blue Tea", "12.000"],
+      ])
+      .newline();
+    basePrint
+      .newline()
+      .line("Nomor Antrian")
+      .line("1")
+      .newline()
+      .line("Untuk cek pesanan kamu, bisa melakukan scan disini");
+    basePrint.qrcode("Guest", 1, 2, "q");
+
+    basePrint.newline().line("Terima Kasih").newline();
+    return basePrint.encode();
   };
+
+  function writeStrToCharacteristic(characteristic, str) {
+    let buffer = new ArrayBuffer(str.length);
+    let dataView = new DataView(buffer);
+    for (var i = 0; i < str.length; i++) {
+      dataView.setUint8(i, str.at(i));
+    }
+    return characteristic.writeValue(dataView);
+  }
 
   const handlePrint = async () => {
     try {
@@ -188,11 +214,31 @@ const Transaction = ({ idtrans, user, setIsupdate }) => {
             "000018f0-0000-1000-8000-00805f9b34fb"
           );
           if (service) {
+            const printData = getData();
+            console.log("printData", printData);
             const characteristic = await service.getCharacteristic(
               "00002af1-0000-1000-8000-00805f9b34fb"
             );
-            const response = await sendPrintData(characteristic);
-            console.log("print result:", response);
+            if (printData.length > MAX_DATA_SIZE) {
+              let j = 0;
+              for (var i = 0; i < printData.length; i += MAX_DATA_SIZE) {
+                var subStr;
+                if (i + MAX_DATA_SIZE <= printData.length) {
+                  subStr = printData.slice(i, i + MAX_DATA_SIZE);
+                } else {
+                  subStr = printData.slice(i, printData.length);
+                }
+                setTimeout(
+                  writeStrToCharacteristic,
+                  250 * j,
+                  characteristic,
+                  subStr
+                );
+                j++;
+              }
+            } else {
+              await characteristic.writeValue(printData);
+            }
           } else {
             console.log("service not found!");
           }
@@ -207,7 +253,6 @@ const Transaction = ({ idtrans, user, setIsupdate }) => {
     }
   };
 
-   console.log("transaksi", currentTrans);
   // console.log('ini data asd', idtrans);
   return (
     <React.Fragment>
