827577ac3028f645cb730b4d711b7c86cfd86c9f
Farihul Rouf <farihulrouf@gmail.com>
2024-02-29 11:26:25 +0700

    text

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -123,14 +123,14 @@ const Scanner = (props) => {
         */}
         <div className="flex space-x-2 justify-between w-1/2 rounded-lg">
           <button
-            className="rounded-l-lg hover:bg-blue-500 hover:-text-white py-2 px-1 flex gap-2 items-center"
+            className="rounded-l-lg py-2 px-1 flex gap-2 items-center hover:bg-blue-500 hover:text-white"
             onClick={() => handleClickAdvanced()}
           >
             <MdQrCodeScanner />
             <span className="text-sm">Scan {props.type}</span>
           </button>
           <button
-            className="py-2 px-1 rounded-r-lg hover:bg-red-500 hover:-text-white text-sm flex gap-2 items-center"
+            className="py-2 px-1 rounded-r-lg text-sm flex gap-2 items-center hover:bg-red-500 hover:text-white"
             onClick={() => handleStop()}
           >
             <FaRegStopCircle />
